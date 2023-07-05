c8b52e260113610e2efa83e659950857ebf11826
matias millacura <mmillacura2020@alu.uct.cl>
2023-07-04 22:47:21 -0400

    arreglar grafico y tamos
    falta estudiar nada mas

--- algoritmo genetico general.py
+++ algoritmo genetico general.py
@@ -14,6 +14,10 @@ color_fondo = [255, 255, 255]
 num_generaciones = 20
 num_generacion = 1 #es para el conteo de las generaciones y que este conteo sea global
 num_salto_generaciones = 5
+# Para almacenar los datos requeridos durante las generaciones
+individuos_en_meta_generaciones = []
+pasos_promedio_generaciones = []
+pasos_primer_en_meta_generaciones = []
 
 class Individuo:
     def __init__(self, x, y, probs, color):
@@ -49,9 +53,9 @@ def cruzar_un_punto(ind1, ind2):
     new_probs2 = np.concatenate((ind2.probs[:punto_cruce], ind1.probs[punto_cruce:]))
 
     # Cambiamos las posiciones iniciales de los hijos
-    x1 = np.random.randint(0, grida_dim//4)
+    x1 = np.random.randint(0, grida_dim//10)
     y1 = np.random.randint(0, grida_dim)
-    x2 = np.random.randint(0, grida_dim//4)
+    x2 = np.random.randint(0, grida_dim//10)
     y2 = np.random.randint(0, grida_dim)
 
     hijos = [Individuo(x1, y1, new_probs1, ind1.color), Individuo(x2, y2, new_probs2, ind2.color)]
@@ -68,7 +72,7 @@ def mutar(individuo, tasa_mutacion=0.1):
 seed = 1
 np.random.seed(seed)
 for _ in range(num_individuos):
-    x = np.random.randint(0, grida_dim//4)
+    x = np.random.randint(0, grida_dim//10)
     y = np.random.randint(0, grida_dim)
     probs = np.random.rand(len(movimientos))
     probs = probs / np.sum(probs)
@@ -158,23 +162,51 @@ def ejecutar_generacion(individuos):
 # Al inicio, creamos los individuos aleatoriamente
 individuos = []
 for _ in range(num_individuos):
-    x = np.random.randint(0, grida_dim//4)
+    x = np.random.randint(0, grida_dim//10)
     y = np.random.randint(0, grida_dim)
     probs = np.random.rand(len(movimientos))
     probs = probs / np.sum(probs)
     movimiento_predominante = np.argmax(probs)
     individuo = Individuo(x, y, probs, colores[movimiento_predominante])
     individuos.append(individuo)
+# Para almacenar los datos requeridos durante las generaciones
+individuos_en_meta_generaciones = []
+pasos_promedio_generaciones = []
+pasos_primer_en_meta_generaciones = []
 
+
+#bucle principal
 for i in range(num_generaciones):
     num_generacion = i + 1 
     print(f"Ejecutando generación {i+1}")
-    individuos_en_meta, primer_en_meta = ejecutar_generacion(individuos)
     
+    while True: # Repite la generación hasta que al menos dos individuos alcancen la meta
+        individuos_en_meta, primer_en_meta = ejecutar_generacion(individuos)
+
+        if len(individuos_en_meta) >= 2:  # Si al menos dos individuos llegaron a la meta, sale del loop
+            break
+        else:
+            print(f"Sólo {len(individuos_en_meta)} individuos alcanzaron la meta. Repitiendo la generación.")
+
+    # Guardando la información requerida de esta generación
+    individuos_en_meta_generaciones.append(len(individuos_en_meta))
+    if len(individuos_en_meta) > 0:
+        pasos_promedio_generaciones.append(np.mean([ind.pasos for ind in individuos_en_meta]))
+        pasos_primer_en_meta_generaciones.append(primer_en_meta.pasos)
+    else:
+        pasos_promedio_generaciones.append(0)
+        pasos_primer_en_meta_generaciones.append(0)
+
+    print(f"Individuos que alcanzaron la meta: {len(individuos_en_meta)}")
+    if len(individuos_en_meta) > 0:
+        probabilidades1 = probabilidades(individuos_en_meta)
+        for i, ind in enumerate(individuos_en_meta):
+            print(f"Individuo {i+1}: Fitness: {ind.fitness()}, Pasos: {ind.pasos}, Probabilidad de ser seleccionado: {probabilidades1[i]}")
+
     if len(individuos_en_meta) > 0:
         nuevos_individuos = []
         if primer_en_meta is not None:
-            x = np.random.randint(0, grida_dim//4)
+            x = np.random.randint(0, grida_dim//10)
             y = np.random.randint(0, grida_dim)
             nuevos_individuos.append(Individuo(x, y, primer_en_meta.probs.copy(), (0, 0, 255)))
         while len(nuevos_individuos) < num_individuos:
@@ -189,13 +221,42 @@ for i in range(num_generaciones):
         print("Ningún individuo alcanzó la meta en la generación actual.")
 
 
+
+plt.ioff()
+plt.show() 
+
+generaciones = list(range(1, num_generaciones + 1))
+
+# Gráfico 1: cantidad de individuos que llegan con respecto a la generación actual
+plt.figure()
+plt.plot(generaciones, individuos_en_meta_generaciones)
+plt.title('Cantidad de individuos que llegan por generación')
+plt.xlabel('Generación')
+plt.ylabel('Cantidad de individuos que llegan')
+plt.show()
+
+# Gráfico 2: pasos promedios de TODOS los individuos que llegan a la meta(borde derecho) con respecto a su generación(grafico de barras)
+plt.figure()
+plt.bar(generaciones, pasos_promedio_generaciones)
+plt.title('Pasos promedio de individuos que llegan por generación')
+plt.xlabel('Generación')
+plt.ylabel('Pasos promedio')
+plt.show()
+
+# Gráfico 3: cantidad de pasos del primero de cada generación(haz un grafico de barras)
+plt.figure()
+plt.bar(generaciones, pasos_primer_en_meta_generaciones)
+plt.title('Cantidad de pasos del primero en llegar por generación')
+plt.xlabel('Generación')
+plt.ylabel('Cantidad de pasos del primero')
+plt.show()
+
 plt.ioff()
 plt.show() 
 
 print(f"Individuos que alcanzaron la meta: {len(individuos_en_meta)}")
-probabilidades = probabilidades(individuos_en_meta)
+probabilidades1 = probabilidades(individuos_en_meta)
 
 for i, ind in enumerate(individuos_en_meta):
-    print(f"Individuo {i+1}: Fitness: {ind.fitness()}, Pasos: {ind.pasos}, Probabilidad de ser seleccionado: {probabilidades[i]}")
+    print(f"Individuo {i+1}: Fitness: {ind.fitness()}, Pasos: {ind.pasos}, Probabilidad de ser seleccionado: {probabilidades1[i]}")
 
-#grafico de la generacion con respecto al promedio de fitness
